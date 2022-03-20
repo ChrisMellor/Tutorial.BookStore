c868c249daac846fbfa4cbac8369d26bcfde3503
Christopher Stuart Mellor <Chris.Mellor@hotmail.co.uk>
2022-03-20 01:14:15 +0000

    Created pages and js files, translations

---
+++ src/Tutorial.BookStore.Web/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tutorial.BookStore.Web.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
